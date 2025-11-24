47cf415afffde1164bfde699b84fa2011213a840
Janeshobito <140884146+Janeshobito@users.noreply.github.com>
2025-11-24 13:16:49 +0530

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,28 @@
+const movies = [
+  {
+    title: "Deadpool & Wolverine",
+    director: "Shawn Levy",
+    release: "July 26, 2024",
+    releaseNote: "Deadpool 3 is scheduled to release on July 26, 2024. It was previously scheduled for September 6 of that year, and then November 8. It will be part of Phase Five of the MCU.",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/8cdWjvZQUExUUTzyp4t6EDMUBj4.jpg",
+    hero: "https://deadline.com/wp-content/uploads/2024/04/deadpool-and-wolverine.jpg?w=1024",
+    cast: [
+      {actor:"Ryan Reynolds", character:"Wade Wilson / Deadpool"},
+      {actor:"Hugh Jackman", character:"Logan / Wolverine"},
+      {actor:"Emma Corrin", character:"Cassandra Nova"},
+      {actor:"Morena Baccarin", character:"Vanessa"},
+      {actor:"Leslie Uggams", character:"Blind Al"},
+      {actor:"Karan Soni", character:"Dopinder"}
+    ],
+    production: "Directed by Shawn Levy<br>Written by Rhett Reese, Paul Wernick, Zeb Wells, Ryan Reynolds & Shawn Levy<br>Produced by Marvel Studios<br>Distributed by Walt Disney Studios Motion Pictures",
+    boxOffice: "$1.338 billion",
+    boxDetails: "Deadpool & Wolverine became the highest-grossing R-rated film of all time, the biggest July opening ever, and the fastest R-rated film to $1 billion.",
+    gallery: [
+      "https://image.tmdb.org/t/p/w1280/yYG7Rhn9HfQu3kmcIy9w0p3j5F4yFzG4v5vG.jpg",
+      "https://image.tmdb.org/t/p/w1280/3N1Shu9t1Ny0p3j5F4yFzG4v5vG.jpg",
+      "https://image.tmdb.org/t/p/w1280/hU1Q9FIhcqR3td4iG0rv3A6k9hJ.jpg",
+      "https://image.tmdb.org/t/p/w1280/4Z0m1sJd7qj2sW0B0NA3iIPbbbs.jpg"
+    ]
+  },
+  // Add more movies here exactly the same way
+];
